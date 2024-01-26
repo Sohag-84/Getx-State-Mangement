2d434378b7f91baddcf70d7526deda41c58f8b2b
Sohag-84 <ihsohag84@gmail.com>
2024-01-26 10:51:28 +0600

    flutter getx

---
+++ e_commerce/lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import '../modules/bottom_nav_bar/bindings/bottom_nav_bar_binding.dart';
+import '../modules/bottom_nav_bar/views/bottom_nav_bar_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.BOTTOM_NAV_BAR,
+      page: () => const BottomNavBarView(),
+      binding: BottomNavBarBinding(),
+    ),
+  ];
+}
