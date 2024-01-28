18f250fa2dd23e6f66c03f93fd4d7ed7d09e4087
Sohag-84 <ihsohag84@gmail.com>
2024-01-28 10:55:18 +0600

    create checkout container

--- e_commerce/lib/app/modules/cart/views/cart_view.dart
+++ e_commerce/lib/app/modules/cart/views/cart_view.dart
@@ -1,17 +1,20 @@
 import 'package:e_commerce/constant/app_colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../controllers/cart_controller.dart';
+import '../widgets/cart_item.dart';
+import '../widgets/checkout_container.dart';
 
 class CartView extends GetView<CartController> {
   const CartView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.bgColor,
       appBar: AppBar(
+        backgroundColor: AppColors.bgColor,
         title: Text(
           'Shopping Cart (4)',
           style: TextStyle(
@@ -20,6 +23,10 @@ class CartView extends GetView<CartController> {
           ),
         ),
         centerTitle: true,
+      ),
+      bottomNavigationBar: checkoutContainer(
+        subtotal: "123",
+        onTap: () {},
       ),
       body: Padding(
         padding: EdgeInsets.all(10.w),
@@ -47,92 +54,6 @@ class CartView extends GetView<CartController> {
           ],
         ),
       ),
-    );
-  }
-
-  Widget cartItem({
-    required VoidCallback incrementTap,
-    required VoidCallback decrementTap,
-    required VoidCallback deleteTap,
-    required String proImage,
-    required String proName,
-    required int cartItemCount,
-    required String proPrice,
-  }) {
-    return Row(
-      children: [
-        Image.asset(
-          proImage,
-          width: 60.w,
-          fit: BoxFit.fill,
-        ),
-
-        SizedBox(width: 5.w),
-
-        ///product name || item increment & decrement
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                proName,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-               SizedBox(height: 5.w),
-              Container(
-                width: 150.w,
-                height: 30.h,
-                padding: EdgeInsets.symmetric(horizontal: 5.w),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12.r),
-                  border: Border.all(
-                    color: AppColors.redAccentColor,
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: decrementTap,
-                      child: const Icon(CupertinoIcons.minus),
-                    ),
-                    Text(cartItemCount.toString()),
-                    InkWell(
-                      onTap: incrementTap,
-                      child: const Icon(Icons.add),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-
-        ///price || delete button
-        Column(
-          children: [
-            Text(
-              proPrice,
-              style: TextStyle(
-                fontSize: 16.sp,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            IconButton(
-              onPressed: deleteTap,
-              icon: Icon(
-                Icons.delete,
-                color: AppColors.redAccentColor,
-              ),
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
