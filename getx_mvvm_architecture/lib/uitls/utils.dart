f81f2f8e0441e56e4c9501fab92028809c52da18
Sohag-84 <ihsohag84@gmail.com>
2024-03-26 12:52:16 +0600

    create app utils class

---
+++ getx_mvvm_architecture/lib/uitls/utils.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:getx_mvvm_architecture/res/routes/colors/app_colors.dart';
+
+class Utils {
+  ///to change textfield keyboard [focus]
+  static void fieldFocusChnage(
+    BuildContext context,
+    FocusNode current,
+    FocusNode nextFocus,
+  ) {
+    current.unfocus();
+    Focus.of(context).requestFocus(nextFocus);
+  }
+
+  ///for show [toast] message
+  static showToast({required String msg}) {
+    return Fluttertoast.showToast(
+      msg: msg,
+      backgroundColor: AppColors.blackColor,
+    );
+  }
+
+  ///for [snackbar]
+  static snackBar({required String title, required String message}) {
+    return Get.snackbar(title, message);
+  }
+}
